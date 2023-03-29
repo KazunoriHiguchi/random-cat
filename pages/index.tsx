d1248f76cfa8e1255bda0e250dcf536cd569de9a
KazunoriHiguchi <kazunorihiguchi@keio.jp>
2023-03-29 21:34:14 +0900

    fin cat generator with typescript and next.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,52 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage, GetServerSideProps } from 'next';
+import { useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
 
-export default IndexPage
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  // useStateを使って状態を定義する
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+  //ボタンをクリックしたときに画像を読み込む処理
+  const handleClick = async () => {
+    setLoading(true); // 読み込み中フラグを立てる
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url); // 画像URLの状態を更新する
+    setLoading(false); // 読み込み中フラグを倒す
+  };
+  // ローディング中でなければ、画像を表示する
+  return (
+    <div className={styles.page} >
+      <h1>りおぴんへ</h1>
+      <h3>猫ちゃんをみて疲れを癒してね</h3>
+      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+      <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
+    </div>
+  );
+};
+export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images: unknown = await res.json();
+  console.log(images);
+  return images[0];
+};
